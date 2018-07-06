42d67ba4353326793baf2d3541b94d95cfc515af
Laiqa Shariff <laiqa@sas.upenn.edu>
2018-07-06 20:47:58 +0000

    completed items show templates

--- tour-builder/tours/show.php
+++ tour-builder/tours/show.php
@@ -1,177 +1,135 @@
 <?php
-$locations=array();
-$tourTitle = (tour( 'title' )) ? strip_formatting( tour( 'title' ) ) : '[Untitled]';
-echo head( array( 'maptype'=>'tour','title' => ''.__('Tour').' | '.$tourTitle, 'bodyid'=>'tours',
-   'bodyclass' => 'show', 'tour'=>$tour ) );
+$maptype='tour';
+$tourTitle = strip_formatting( tour( 'title' ) );
+$label = mh_tour_label();
+if( $tourTitle != '' && $tourTitle != '[Untitled]' ) {
+} else {
+    $tourTitle = '';
+}
+echo head( array( 'maptype'=>$maptype,'title' => ''.$label.' | '.$tourTitle, 'content_class' => 'horizontal-nav', 'bodyid'=>'tours',
+'bodyclass' => 'show tour', 'tour'=>$tour, 'context'=>'standard' ) );
 ?>
-    <!-- Page Content -->
-    <div class="container">
 
-      <div class="row">
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
 
         <!-- Post Content Column -->
         <div class="col-lg-8">
 
-          <!-- Title -->
-          <h1 class="mt-4">Browse Single Tour</h1>
+            <!-- Title -->
+            <h1 class="mt-4"><?php echo $tourTitle; ?></h1>
 
-          <!-- Author -->
-        <!--  <p class="lead">
-            by
-            <a href="#">Start Bootstrap</a>
-          </p>
-          <hr>
+            <!-- Author -->
+            <?php $byline=null;
+            if(tour( 'Credits' )){
+                $byline.= '<span class="tour-meta">'.__(' %s',tour( 'Credits' )).'</span>';
+            }elseif(get_theme_option('show_author') == true){
+                $byline.= '<span class="tour-meta">'.__(' The %s Team',option('site_title')).'</span>';
+            }
+            ?>
 
-        Date/Time
-          <p>Posted on January 1, 2018 at 12:00 PM</p>
+            <p class="lead">
+                by <?php echo $byline ?>
+            </p>
 
-          <hr> -->
+            <hr>
 
-          <!-- Preview Image -->
-          <!--<img class="img-fluid rounded" src="https://ibb.co/mLBkny" alt="">-->
-            <a href="https://ibb.co/mLBkny"><img src="https://preview.ibb.co/eteufJ/otter.jpg" alt="otter" border="0" height="250" width="730"/></a>
+            <!-- Preview Image -->
+            <section class="map">
+                <figure>
+                    <?php echo mh_map_type($maptype,null,$tour); ?>
+                    <?php echo mh_map_actions(null,$tour);?>
+                </figure>
+            </section>
 
-          <hr>
+            <hr>
 
-          <!-- Post Content -->
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-
-          <h2>Locations for Tour</h2>
-          <div class="mapouter"><div class="gmap_canvas"><iframe width="730" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=schuylkill%20river%20philadelphia&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de">webdesign: pureblack.de</a></div><style>.mapouter{text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:730px;}</style></div>
-          <br>
-
-          <blockquote class="blockquote">
-            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">Someone famous in
-              <cite title="Source Title">Source Title</cite>
-            </footer>
-          </blockquote>
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
-
-          <hr>
-
-          <!-- Comments Form -->
-          <div class="card my-4">
-            <h5 class="card-header">Leave a Comment:</h5>
-            <div class="card-body">
-              <form>
-                <div class="form-group">
-                  <textarea class="form-control" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
+            <!-- Post Content -->
+            <div class="lead">
+                <?php echo htmlspecialchars_decode(nls2p( tour( 'Description' ) )); ?>
             </div>
-          </div>
 
-          <!-- Single Comment -->
-          <div class="media mb-4">
-            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-            <div class="media-body">
-              <h5 class="mt-0">Commenter Name</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            <hr>
+
+            <section id="tour-items">
+
+                <h3 ><?php echo __('Locations for %s', $label);?></h3>
+                <?php
+                $i=1;
+                foreach( $tour->getItems() as $tourItem ):
+                    if($tourItem->public):
+                        set_current_record( 'item', $tourItem );
+                        $itemID=$tourItem->id;
+                        $hasImage=metadata($tourItem,'has thumbnail');
+                        ?>
+                        <div class="tour-item-container flex">
+                            <div class="tour-route flex flex-column" aria-hidden="true">
+                                <div class="tour-route-number"><?php echo $i;?> </div>
+                                <div class="tour-route-path"></div>
+                            </div>
+                            <article class="item-result <?php echo $hasImage ? 'has-image' : null;?>">
+                                <h3><a href="<?php echo url('/') ?>items/show/<?php echo $itemID.'?tour='.tour( 'id' ).'&index='.($i-1).''; ?>">
+                                    <?php echo metadata( $tourItem, array('Dublin Core', 'Title') ); ?>
+                                </a></h3>
+
+                                <div class="item-description"><?php echo snippet(mh_the_text($tourItem),0,250); ?></div>
+                            </article>
+                        </div>
+                        <?php
+                        $i++;
+                        $item_image=null;
+                    endif;
+                endforeach; ?>
+            </section>
+
+            <hr>
+
+            <section id="tour-postscript">
+                <?php echo htmlspecialchars_decode(metadata('tour','Postscript Text')); ?>
+            </section>
+
+            <div class="comments">
+                <?php echo (get_theme_option('tour_comments') ==1) ? mh_display_comments() : null;?>
             </div>
-          </div>
-
-          <!-- Comment with nested comments -->
-          <div class="media mb-4">
-            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-            <div class="media-body">
-              <h5 class="mt-0">Commenter Name</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-
-              <div class="media mt-4">
-                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-                <div class="media-body">
-                  <h5 class="mt-0">Commenter Name</h5>
-                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-              </div>
-
-              <div class="media mt-4">
-                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-                <div class="media-body">
-                  <h5 class="mt-0">Commenter Name</h5>
-                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-              </div>
-
-            </div>
-          </div>
 
         </div>
 
         <!-- Sidebar Widgets Column -->
         <div class="col-md-4">
 
-          <!-- Search Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Search</h5>
-            <div class="card-body">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search for...">
-                <span class="input-group-btn">
-                  <button class="btn btn-secondary" type="button">Go!</button>
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <!-- Categories Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Categories</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">All Tours</a>
-                    </li>
-                    <li>
-                      <a href="#">Tour Stops</a>
-                    </li>
-                    <li>
-                      <a href="#">Single Stops</a>
-                    </li>
-                  </ul>
+            <!-- Search Widget -->
+            <div class="card my-4">
+                <h5 class="card-header">Search</h5>
+                <div class="card-body">
+                    <?php echo search_form(); ?>
                 </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Oil Refinery Tour</a>
-                    </li>
-                    <li>
-                      <a href="#">River Intern Center City Voyage</a>
-                    </li>
-                    <li>
-                      <a href="#">Tour 3</a>
-                    </li>
-                  </ul>
+            </div>
+
+            <!-- Tours Widget -->
+            <div class="card my-4">
+                <h5 class="card-header">Tours</h5>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <ul class="list-unstyled mb-0">
+                                <?php set_loop_records("tours", get_records("Tour")); ?>
+                                <?php foreach (loop("tours") as $otherTour): ?>
+                                    <?php set_current_record( 'tour', $otherTour ); ?>
+                                    <li>
+                                        <?php echo link_to_tour(null,array('class'=>'permalink')); ?>
+                                    </li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-
-          <!-- Side Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Side Widget</h5>
-            <div class="card-body">
-              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-            </div>
-          </div>
-
         </div>
 
-      </div>
-      <!-- /.row -->
-
     </div>
-    <!-- /.container -->
+    <!-- /.row -->
 
-<?php echo foot(); ?>
+</div>
+<!-- /.container -->
